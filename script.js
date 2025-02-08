b22a8e7ddba59105f878bced0bfa33e3e191a82e
L0uFish <63689247+L0uFish@users.noreply.github.com>
2025-02-08 12:34:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -104,7 +104,7 @@ document.addEventListener("DOMContentLoaded", function () {
 		label.textContent = formattedDate;
 	  });
     chrome.runtime.sendMessage(
-      "fjgodpiekafafncggbbagckcmgjnnbmk",
+      "adfmkooomcpdikkgalpdmjfhjlciemlj",
       { type: "getExtractedData" },
       function (response) {
         console.log("Data received:", response);
